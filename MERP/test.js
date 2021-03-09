66e9c50a14e34164703631f0df21af32858a25d0
LyricalStreetPoet <32119569+LyricalStreetPoet@users.noreply.github.com>
2021-03-09 09:21:17 -0600

    Complete Rewrite
    Lots of automation and improvements

---
+++ MERP/test.js
@@ -0,0 +1,78 @@
+        case "Club":
+            bonuses["weapon_fumble"] = 4;
+            bonuses["weapon_fumble_critical"] = "";
+            bonuses["weapon_primary_critical"] = "Crush(D)";
+            bonuses["weapon_secondary_critical"] = "";
+            bonuses["weapon_range"] = 2;
+            bonuses["weapon_reload_rounds"] = 0;
+            bonuses["weapon_reload_penalty"] = 0;
+            bonuses["weapon_weight"] = 5;
+            bonuses["weapon_noleather_bonus"] = -10;
+            bonuses["weapon_chainplate_bonus"] = -10;
+            bonuses["weapon_skill_bonus_type"] = "1conc";
+            break;
+        case "Mace":
+            bonuses["weapon_fumble"] = 2;
+            bonuses["weapon_fumble_critical"] = "";
+            bonuses["weapon_primary_critical"] = "Crush";
+            bonuses["weapon_secondary_critical"] = "";
+            bonuses["weapon_range"] = 5;
+            bonuses["weapon_reload_rounds"] = 0;
+            bonuses["weapon_reload_penalty"] = 0;
+            bonuses["weapon_weight"] = 5;
+            bonuses["weapon_noleather_bonus"] = 0;
+            bonuses["weapon_chainplate_bonus"] = 0;
+            bonuses["weapon_skill_bonus_type"] = "1conc";
+            break;
+        case "Morning Star":
+            bonuses["weapon_fumble"] = 8;
+            bonuses["weapon_fumble_critical"] = "Crush(B)";
+            bonuses["weapon_primary_critical"] = "Crush";
+            bonuses["weapon_secondary_critical"] = "Puncture(A)";
+            bonuses["weapon_range"] = 0;
+            bonuses["weapon_reload_rounds"] = 0;
+            bonuses["weapon_reload_penalty"] = 0;
+            bonuses["weapon_weight"] = 5;
+            bonuses["weapon_noleather_bonus"] = 10;
+            bonuses["weapon_chainplate_bonus"] = 10;
+            bonuses["weapon_skill_bonus_type"] = "1conc";
+            break;
+        case "Net":
+            bonuses["weapon_fumble"] = 6;
+            bonuses["weapon_fumble_critical"] = "";
+            bonuses["weapon_primary_critical"] = "Grapple";
+            bonuses["weapon_secondary_critical"] = "";
+            bonuses["weapon_range"] = 10;
+            bonuses["weapon_reload_rounds"] = 0;
+            bonuses["weapon_reload_penalty"] = 0;
+            bonuses["weapon_weight"] = 3;
+            bonuses["weapon_noleather_bonus"] = -10;
+            bonuses["weapon_chainplate_bonus"] = 15;
+            bonuses["weapon_skill_bonus_type"] = "1conc";
+            break;
+        case "War Hammer":
+            bonuses["weapon_fumble"] = 4;
+            bonuses["weapon_fumble_critical"] = "";
+            bonuses["weapon_primary_critical"] = "Crush";
+            bonuses["weapon_secondary_critical"] = "";
+            bonuses["weapon_range"] = 10;
+            bonuses["weapon_reload_rounds"] = 0;
+            bonuses["weapon_reload_penalty"] = 0;
+            bonuses["weapon_weight"] = 5;
+            bonuses["weapon_noleather_bonus"] = 5;
+            bonuses["weapon_chainplate_bonus"] = 5;
+            bonuses["weapon_skill_bonus_type"] = "1conc";
+            break;
+        case "Whip"
+            bonuses["weapon_fumble"] = 6;
+            bonuses["weapon_fumble_critical"] = "";
+            bonuses["weapon_primary_critical"] = "Grapple(C)";
+            bonuses["weapon_secondary_critical"] = "Slash(A)";
+            bonuses["weapon_range"] = 0;
+            bonuses["weapon_reload_rounds"] = 0;
+            bonuses["weapon_reload_penalty"] = 0;
+            bonuses["weapon_weight"] = 3;
+            bonuses["weapon_noleather_bonus"] = -10;
+            bonuses["weapon_chainplate_bonus"] = -10;
+            bonuses["weapon_skill_bonus_type"] = "1conc";
+            break;
